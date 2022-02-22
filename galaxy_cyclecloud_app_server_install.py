a0d37ee12c4195f0741409c6c188f75127478ff1
rafdesouza <36763322+rafdesouza@users.noreply.github.com>
2022-02-22 22:38:46 +0800

    Update galaxy_cyclecloud_app_server_install.py

--- galaxy_cyclecloud_app_server_install.py
+++ galaxy_cyclecloud_app_server_install.py
@@ -257,7 +257,7 @@ def cyclecloud_account_setup(vm_metadata, use_managed_identity, tenant_id, appli
             print("Registering Azure subscription in CycleCloud")
             try:
                 cmd_list = ["/usr/local/bin/cyclecloud", "account", "create", "-f", azure_data_file]
-                output = subprocess.run(cmd_list, stdout=PIPE, stderr=PIPE)
+                output = subprocess.run(cmd_list, capture_out=True).stdout
                 print("Command list:", cmd_list)
                 print("Command output:", output)
             except CalledProcessError as e:
